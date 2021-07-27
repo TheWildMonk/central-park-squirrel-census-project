dcb478fd6911fde4902be323790ae9f3ea8c8f5b
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-27 21:53:26 +0600

    finalize the project

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import pandas as pd
 
 data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-fur_color = data["Primary Fur Color"].to_list()
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 
-gray = 0
-red = 0
-black = 0
-for each_color in fur_color:
-    if each_color == "Gray":
-        gray += 1
-    elif each_color == "Cinnamon":
-        red += 1
-    elif each_color == "Black":
-        black += 1
 
 data_dict = {
     "Fur Color": ["gray", "red", "black"],
-    "Count": [gray, red, black]
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
 squirrel_count = pd.DataFrame(data_dict)
 squirrel_count.to_csv("squirrel_count.csv")
